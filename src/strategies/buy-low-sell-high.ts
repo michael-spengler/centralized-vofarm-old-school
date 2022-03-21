98236432a61f68c36af7b38d7312c23b0e5d7c79
Michael <michael.spengler@sap.com>
2022-03-21 10:49:36 +0100

    f

--- src/strategies/buy-low-sell-high.ts
+++ src/strategies/buy-low-sell-high.ts
@@ -40,7 +40,7 @@ export class BuyLowSellHigh extends VoFarmStrategy {
 
         this.enrichPortfolioInsights()
 
-        if (this.positionInsights[0].sma.length > this.historyLength) {
+        if (this.positionInsights[0].sma.length === this.historyLength) {
             this.executeBuyLowSellHigh()
         } else {
             console.log(this.positionInsights[0].sma.length)
